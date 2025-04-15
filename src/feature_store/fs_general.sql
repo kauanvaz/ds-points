c0f374e87ba48db5bd692e7cf9cba0fa37dd6db8
Kauan Vaz <kauanvazc@gmail.com~>
2025-04-14 22:46:56 -0300

    feat: Feature Store Pontos

--- src/feature_store/fs_general.sql
+++ src/feature_store/fs_general.sql
@@ -4,12 +4,16 @@ WITH tb_rfv AS (
 
     SELECT
         idCustomer,
+
         CAST(MIN(julianday('{date}') - julianday(dtTransaction))
             AS INT) + 1 AS recenciaDias,
+
         COUNT(DISTINCT DATE(dtTransaction)) AS frequenciaDias,
+
         SUM(CASE
                 WHEN pointsTransaction > 0 THEN pointsTransaction 
             END) AS valorPoints
+
     FROM transactions
     WHERE dtTransaction < '{date}'
         AND dtTransaction > DATE('{date}', '-21 days')
@@ -21,8 +25,10 @@ tb_idade AS (
 
     SELECT
         t1.idCustomer,
+
         CAST(MAX(julianday('{date}') - julianday(t2.dtTransaction))
             AS INT) + 1 AS idadeBaseDias
+            
     FROM tb_rfv AS t1
     LEFT JOIN transactions AS t2
         ON t1.idCustomer = t2.idCustomer
